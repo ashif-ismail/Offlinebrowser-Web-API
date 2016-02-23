9d2ff440b17a354468914a21287ef690297c6d1c
SheikhZayed <cyber2maro@gmail.com>
2016-02-23 22:57:13 +0530

    added HTML extraction and Extractor choosing dropdown

--- src/me/ashif/servlets/ExtractServlet.java
+++ src/me/ashif/servlets/ExtractServlet.java
@@ -1,7 +1,13 @@
 package me.ashif.servlets;
 
+import de.l3s.boilerpipe.BoilerpipeExtractor;
 import de.l3s.boilerpipe.BoilerpipeProcessingException;
 import de.l3s.boilerpipe.extractors.ArticleExtractor;
+import de.l3s.boilerpipe.extractors.CommonExtractors;
+import de.l3s.boilerpipe.extractors.KeepEverythingExtractor;
+import de.l3s.boilerpipe.sax.HTMLFetcher;
+import de.l3s.boilerpipe.sax.HTMLHighlighter;
+import org.xml.sax.SAXException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -22,19 +28,71 @@ public class ExtractServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         PrintWriter out = response.getWriter();
-//        out.println("URL to extract :" + url);
-        try {
+        String urlString = request.getParameter("url");
+        String listOUtput = request.getParameter("OutputType");
+        URL url = new URL(urlString);
+        switch (listOUtput) {
+            case "1":
+                if(request.getParameter("ExtractorType").equals("1"))
+                    //when the extractor is ArticleExtractor and the
+                    //output mode is Plain Text
+                        try {
+                            String mainArticle = ArticleExtractor.INSTANCE.getText(url);
+                            out.println(mainArticle);
+                        } catch (BoilerpipeProcessingException ex) {
+                            out.println(ex.getMessage());
+                        }
+                else if(request.getParameter("ExtractorType").equals("2"))
+                    //when the extractor is KeepEverythingExtractor and the
+                    //output mode is Plain Text
+                    try {
+                        String fullArticle = KeepEverythingExtractor.INSTANCE.getText(url);
+                        out.println(fullArticle);
+                    } catch (BoilerpipeProcessingException ex) {
+                        out.println(ex.getMessage());
+                    }
+                break;
+            case "2":
+                if(request.getParameter("ExtractorType").equals("1"))
+                    //when the extractor is Article Extractor
+                    //and the output mode is HTML Text
+                    try
+                    {
+                        final BoilerpipeExtractor extractor = CommonExtractors.ARTICLE_EXTRACTOR;
+                        final boolean includeImages = false;
+                        final boolean bodyOnly = true;
+                        final HTMLHighlighter hh = HTMLHighlighter.newExtractingInstance(includeImages, bodyOnly);
+                        out.println("<base href=\"" + url + "\" >");
+                        out.println("<meta http-equiv=\"Content-Type\" content=\"text-html; charset=utf-8\" />");
+                        String extractedHtml = hh.process(url, extractor);
+                        out.println(extractedHtml);
+                    } catch (SAXException | BoilerpipeProcessingException ex) {
+                        out.println(ex.getMessage());
+                    }
 
-            String urlString = request.getParameter("url");
-            URL url = new URL(urlString);
-            String article = ArticleExtractor.INSTANCE.getText(url);
-            out.println(article);
-        } catch (BoilerpipeProcessingException e) {
-            out.println("Sorry We Couldn't Scrape the URL you Entered,Verify that the URL you entered is prefixed with http://");
-        } catch (IOException e) {
-            out.println("Exception thrown" + e);
+                else if(request.getParameter("ExtractorType").equals("2"))
+                    //when the extractor is KeepEverything Extractor
+                    //and the output mode is HTML
+                        try {
+                            final BoilerpipeExtractor extractor = CommonExtractors.LARGEST_CONTENT_EXTRACTOR;
+                            final boolean includeImages = false;
+                            final boolean bodyOnly = true;
+                            final HTMLHighlighter hh = HTMLHighlighter.newExtractingInstance(includeImages, bodyOnly);
+                            out.println("<base href=\"" + url + "\" >");
+                            out.println("<meta http-equiv=\"Content-Type\" content=\"text-html; charset=utf-8\" />");
+                            String extractedHtml = hh.process(url, extractor);
+                            out.println(extractedHtml);
+                        } catch (SAXException | BoilerpipeProcessingException ex) {
+                            out.println(ex.getMessage());
+                        }
+
+                }
         }
-
-
     }
-}
+
+
+
+
+
+
+
--- web/index.jsp
+++ web/index.jsp
@@ -8,12 +8,44 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-  <title>Offline Browser Backed Web API</title>
+  <title>Offline Browser Backend Web API</title>
+  <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src = "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-<h3 align="center">Offline Browser Backed Web API</h3>
+  <div class="jumbotron">
+    <h2>Offline Browser WebText Extraction Engine</h2>
+    <p>Welcome to the Web API for the Offline Browser Android App</p>
+    </br>
+</div>
+<p>This WebApp is Responsible for Removing and Purifying the Web Page from Surples,Unwanted Contents and turning them into Plain Text Form</p>
 <form action="ExtractServlet" method="get">
-  Enter URL:<input type="text" name="url" placeholder="url to extract">
+  Enter URL : <input type="text" name="url" class="mytext" placeholder="http://www.somesite.com">
+  <select name="OutputType" class="div-inline">
+    <option value="1">Plain Text</option>
+    <option value="2">HTML</option>
+  </select>
+  <select name="ExtractorType" class="div-inline">
+    <option value="1">Article Extractor</option>
+    <option value="2">Keep Everything Extractor</option>
+  </select>
+  </br></br>
+  <input type="submit" name="btnSubmit" class="btn-primary">&nbsp;<input type="reset" name="btnReset" class="btn-primary">
 </form>
 </body>
+<style type="text/css">
+  html {
+    text-align: center;
+  }
+  .mytext {
+    width: 300px;
+  }
+  .div-inline{
+    display:inline-block;
+  }
+</style>
+<div class="navbar navbar-fixed-bottom">
+<p>This Project Is Developed Under the Guidance of <strong>Dr.Antony P.J</strong>(Co-ordinator of PG Studies,KVGCE) by <strong>Abdul Ashif</strong> and <strong>Rohan D'Souza</strong></p>
+</div>
 </html>
